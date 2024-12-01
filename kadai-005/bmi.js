94827bb975bc46e74b4d575b0ef4709c6ea3c008
Kei226 <keitu226@yahoo.co.jp>
2024-12-02 08:07:46 +0900

    kadai-005の再提出

--- kadai-005/bmi.js
+++ kadai-005/bmi.js
@@ -4,8 +4,8 @@ let userHeight;
 let userBmi;
 
 //値の代入
-userWeight='68';
-userHeight='1.7';
+userWeight=68;
+userHeight=1.7;
 userBmi=(userWeight/(userHeight*userHeight));
 //コンソールへの出力
 console.log(userBmi);
